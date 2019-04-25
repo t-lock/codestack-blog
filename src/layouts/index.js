ea84d8ce9b59256d2de6d61691e8189a16bb8027
tlockAOB <tlock@armyofbees.com>
2019-04-26 01:12:29 +1200

    kill the dumb turtles thing in the sidebar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,7 +13,10 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="Gatsby Starter"
       meta={[
-        { name: 'description', content: 'This is a sample blog built on Gatsby' },
+        {
+          name: 'description',
+          content: 'This is a sample blog built on Gatsby',
+        },
         { name: 'keywords', content: 'gatsby, react, tlock, awesome' },
       ]}
     />
@@ -31,13 +34,13 @@ const TemplateWrapper = ({ children }) => (
           matches ? (
             <div
               style={{
-                margin: "0 auto",
+                margin: '0 auto',
                 maxWidth: 980,
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                height: "100%",
-                padding: "25px"
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                height: '100%',
+                padding: '25px',
               }}
             >
               <div style={{ flex: 1 }}>{children()}</div>
@@ -45,26 +48,23 @@ const TemplateWrapper = ({ children }) => (
           ) : (
             <div
               style={{
-                margin: "0 auto",
+                margin: '0 auto',
                 maxWidth: 980,
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                height: "100%",
-                padding: "25px"
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                height: '100%',
+                padding: '25px',
               }}
             >
-              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+              <div style={{ flex: 2.5, paddingRight: '30px' }}>
                 {children()}
               </div>
               <div style={{ flex: 1 }}>
+                <Sidebar title="Aside" description="Tlock is the lorem ipsum" />
                 <Sidebar
-                  title="Aside"
-                  description="Tlock is the lorem ipsum"
-                />
-                <Sidebar
-                  title="About author"
-                  description="I like turtles..."
+                  title="👷‍♂️"
+                  description="Under construction... its really bad rn so..."
                 />
               </div>
             </div>
